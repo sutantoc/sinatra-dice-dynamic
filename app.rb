d8bf7cf1334e1bbdb8041e0fd5d96b4c6571a514
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-04-18 01:22:17 +0000

    num dice

--- app.rb
+++ app.rb
@@ -54,9 +54,10 @@ get("/dice/5/4") do
 end
 
 get("/dynamic/:number_of_dice/6") do
+  @num_dice = params.fetch("number_of_dice").to_i
   @rolls = []
 
-  50.times do
+  @num_dice.times do
     die = rand(1..6)
     @rolls.push(die)
   end
